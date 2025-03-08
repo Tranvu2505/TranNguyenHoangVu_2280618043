30182ba6ad21e6874f9fd611dbd31b3389627569
TranNguyenHoangVu_2280618043 <tranvu.2004bl@gmail.com>
2025-03-08 15:07:10 +0700

    [add] lab-02 ex01 caesar

--- TranNguyenHoangVu_2280618043/LAB_01/b5.py
+++
@@ -1,6 +0,0 @@
-so_gio_lam = float(input("nhập số giờ làm mỗi tuần: "))
-luong_gio = float(input("nhập thù lao trên mỗi giờ làm tiên chuẩn: "))
-gio_tieu_chuan = 44
-gio_vuot_chuan = max(0, so_gio_lam - gio_tieu_chuan)
-thuc_linh = gio_tieu_chuan* luong_gio + gio_tieu_chuan* luong_gio*1.5
-print(f"số tiền thực ling=hx của nhân viên: {thuc_linh}")
